63ff0d44cb62c16fb42e35d976dccfb48bee34f6
Cartodream <90085887+Cartodream@users.noreply.github.com>
2021-11-27 08:02:44 +0100

    Add files via upload

---
+++ donnees/circuits_vtt.js
@@ -0,0 +1,8 @@
+var vtt = {
+"type": "FeatureCollection",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+
+"features": [
+{ "type": "Feature", "properties": { "id":"78171000ZD0202","commune":"78171","prefixe":"000","section":"ZD","numero":"202","contenance":87.0,"arpente":"true","created":"2010-04-19","updated":"2019-07-02"}, "geometry": { "type": "Polygon", "coordinates": [ [ [ 1.664593,48.7393397],[1.6644583,48.739387],[1.6643965,48.739296],[1.6644341,48.7392918],[1.6645575,48.7392897],[1.664593,48.7393397] ] ] } }
+]
+};
